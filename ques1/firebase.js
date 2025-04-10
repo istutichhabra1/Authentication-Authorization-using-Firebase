d91de4d304187c77a6b77d1d10f2f9bca0a31a1f
Istuti Chhabra <102655463+istutichhabra1@users.noreply.github.com>
2025-04-10 14:13:34 +0530

    Create firebase.js

---
+++ ques1/firebase.js
@@ -0,0 +1,14 @@
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  databaseURL: "YOUR_DATABASE_URL",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
